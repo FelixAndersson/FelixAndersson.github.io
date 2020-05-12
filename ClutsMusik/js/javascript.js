83725608f26adc0e3f906c73f4f714b8cffe8ba1
FelixAndersson <fele.a@hotmail.se>
2020-05-12 11:20:56 +0200

    c

--- ClutsMusik/js/javascript.js
+++ ClutsMusik/js/javascript.js
@@ -2,15 +2,31 @@ $("#window").css("overflow", "hidden");
 $("#button").show();
 $("#button2").show();
 
-$("#dot1").show();
-$("#dot2").show();
-$("#dot3").show();
+
+var kör;
+kör = setInterval(auto, 5000);
+function auto(){
+  var slider = $("#imgs");
+
+  if (plats==0) {
+    slider.animate({right:"+=804"},1000)
+
+    plats++;
 
 
-document.getElementById("dot3").style.backgroundColor = "lightblue";
-document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
+  }else if (plats==1) {
+    slider.animate({right:"+=804"},1000)
 
+    plats++;
+
+  }else if (plats==2) {
+    slider.animate({right:"-=1608"},1000)
+
+    plats=0;
+
+  }
+  console.log(plats);
+}
 var plats = 0;
 
 $("#button2").click(function(){
@@ -19,128 +35,46 @@ var slider = $("#imgs");
 
 if (plats==0) {
   slider.animate({right:"+=804"},1000)
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
+
   plats++;
 
 
 }else if (plats==1) {
   slider.animate({right:"+=804"},1000)
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "lightblue";
+
   plats++;
 
 }else if (plats==2) {
   slider.animate({right:"-=1608"},1000)
-  document.getElementById("dot3").style.backgroundColor = "lightblue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
+
   plats=0;
 
 }
 console.log(plats);
-//används för att se vilken position/plats jag befinner mig i slidern, för att underlätta arbetet
 });
 $("#button").click(function(){
 
 var slider = $("#imgs");
 if (plats==0) {
   slider.animate({right:"+=1608"},1000)
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "lightblue";
+
 
   plats=2;
 
 
 }else if (plats==1) {
   slider.animate({right:"-=804"},1000)
-  document.getElementById("dot3").style.backgroundColor = "lightblue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
+
 
   plats--;
 
 
 }else if (plats==2) {
   slider.animate({right:"-=804"},1000)
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
+
 
   plats--;
 
 }
 console.log(plats);
-});
-$("#dot3").click(function(){
-  var slider = $("#imgs");
-if(plats==0){
-    slider.animate({right:"0"},1000)
-  document.getElementById("dot3").style.backgroundColor = "lightblue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  plats=0;
-
-}else if(plats==1){
-  slider.animate({right:"-=804"},1000)
-  document.getElementById("dot3").style.backgroundColor = "lightblue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  plats=0;
-}else if(plats==2)
-slider.animate({right:"-=1608"},1000)
-document.getElementById("dot3").style.backgroundColor = "lightblue";
-document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-plats=0;
-
-});
-$("#dot2").click(function(){
-  var slider = $("#imgs");
-if(plats==0){
-    slider.animate({right:"+=804"},1000)
-  document.getElementById("dot2").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  plats=1;
-
-}else if(plats==1){
-  slider.animate({right:"0"},1000)
-  document.getElementById("dot2").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-  plats=1;
-}else if(plats==2)
-slider.animate({right:"-=804"},1000)
-document.getElementById("dot2").style.backgroundColor = "lightblue";
-document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-document.getElementById("dot1").style.backgroundColor = "DodgerBlue";
-plats=1;
-
-});
-$("#dot1").click(function(){
-  var slider = $("#imgs");
-if(plats==0){
-    slider.animate({right:"+=1608"},1000)
-  document.getElementById("dot1").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  plats=2;
-
-}else if(plats==1){
-  slider.animate({right:"+=804"},1000)
-  document.getElementById("dot1").style.backgroundColor = "lightblue";
-  document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-  document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-  plats=2;
-}else if(plats==2)
-slider.animate({right:"0"},1000)
-document.getElementById("dot1").style.backgroundColor = "lightblue";
-document.getElementById("dot3").style.backgroundColor = "DodgerBlue";
-document.getElementById("dot2").style.backgroundColor = "DodgerBlue";
-plats=2;
-
 });
