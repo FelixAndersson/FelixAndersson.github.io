db723127a7d47973886a5e5f87a5eb184eba8fc8
FelixAndersson <fele.a@hotmail.se>
2020-05-24 15:56:49 +0200

    mycket som hände på denna

--- ClutsMusik/js/javascript.js
+++ ClutsMusik/js/javascript.js
@@ -5,27 +5,43 @@ $("#button2").show();
 
 document.getElementById("window").style.display = "block";
 document.getElementById("window").style.overflow = "hidden";
-var kör;
-kör = setInterval(auto, 7000);
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+
+var kor;
+kor = setInterval(auto, 7000);
 function auto(){
   var slider = $("#imgs");
 
   if (plats==0) {
-    var wi = document.getElementById('window').offsetWidth;
-    slider.animate({right:wi},2000)
+    var windowsize = document.getElementById("window").offsetWidth;
+    slider.animate({right:windowsize},2000);
 
     plats++;
 
 
   }else if (plats==1) {
-    var wi = document.getElementById('window').offsetWidth;
-    slider.animate({right:wi+wi},2000)
+    var windowsize = document.getElementById("window").offsetWidth;
+    slider.animate({right:windowsize+windowsize},2000);
 
     plats++;
 
   }else if (plats==2) {
-    var wi = document.getElementById('window').offsetWidth;
-    slider.animate({right:wi-wi},2000)
+    var windowsize = document.getElementById("window").offsetWidth;
+    slider.animate({right:windowsize-windowsize},2000);
 
     plats=0;
 
@@ -34,55 +50,9 @@ function auto(){
 }
 var plats = 0;
 
-$("#button2").click(function(){
-
-var slider = $("#imgs");
-
-if (plats==0) {
-  slider.animate({right:"+=804"},2000)
-
-  plats++;
 
 
-}else if (plats==1) {
-  slider.animate({right:"+=804"},2000)
 
-  plats++;
-
-}else if (plats==2) {
-  slider.animate({right:"-=1608"},2000)
-
-  plats=0;
-
-}
-console.log(plats);
-});
-$("#button").click(function(){
-
-var slider = $("#imgs");
-if (plats==0) {
-  slider.animate({right:"+=1608"},2000)
-
-
-  plats=2;
-
-
-}else if (plats==1) {
-  slider.animate({right:"-=804"},2000)
-
-
-  plats--;
-
-
-}else if (plats==2) {
-  slider.animate({right:"-=804"},2000)
-
-
-  plats--;
-
-}
-console.log(plats);
-});
 
 
 var hand = [];
